9303a7fb0794f477bd7fcb14c18bda72d1418327
muskan2415 <jaspinderkandiara@gmail.com>
2026-01-02 15:19:05 +0530

    Feat: Add Admin Login link to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -84,6 +84,11 @@ const Footer = () => {
                   Contact
                 </Link>
               </li>
+              <li>
+                <Link to="/admin/login" className="text-gray-400 hover:text-primary-orange transition-colors">
+                  Admin Login
+                </Link>
+              </li>
             </ul>
           </div>
 
